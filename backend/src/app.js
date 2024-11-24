34dea3db32f6a5fea4ef2afb9d9f93daeef59e17
Manjeet Kumar <manjeetkumar62054@gmail.com>
2024-11-24 13:38:38 +0530

    m

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://ecm-art-manjeetmathurs-projects.vercel.app/",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
