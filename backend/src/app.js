13e386f604fbc1aaeec4d8b6126a3e10da93c3cb
Manjeet Kumar <manjeetkumar62054@gmail.com>
2024-11-28 15:43:54 +0530

    m

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,8 @@ const __dirname = path.resolve()
 app.use(express.json());
 app.use(
   cors({
-    origin : "https://ecm-art-frontend.onrender.com/",
+    origin : "https://ecm-art-frontend.onrender.com",
+    // origin : "http://localhost:5173",
     credentials: true,
   })
 );
