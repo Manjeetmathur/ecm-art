c206700e362eac4713b5609d568721d08a2a7def
Manjeet Kumar <manjeetkumar62054@gmail.com>
2024-11-28 14:51:27 +0530

    m

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,7 @@ const __dirname = path.resolve()
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     credentials: true,
   })
 );
--- frontend/src/components/bacxkendUrl/BackendUrl.js
+++ frontend/src/components/bacxkendUrl/BackendUrl.js
@@ -1,3 +1,3 @@
-export const url = `http://localhost:8000/api`
+export const url = `https://ecm-art.onrender.com/api`
 //https://ecm-art.onrender.com/api
 //http://localhost:8000/api
