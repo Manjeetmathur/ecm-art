401c64ee0024a10b4debb5fdc891e1027794e3b8
Manjeet Kumar <manjeetkumar62054@gmail.com>
2024-11-28 16:22:02 +0530

    m

--- backend/src/controller/userController.js
+++ backend/src/controller/userController.js
@@ -92,7 +92,7 @@ const login  = asyncHandler(async(req,res) => {
 
       const tokenOption = {
         secure : true,
-        sameStite : "none",
+        sameSite : "none",
         httpOnly:true,
         maxAge : 1 * 24 * 60 * 60 * 1000
       }
